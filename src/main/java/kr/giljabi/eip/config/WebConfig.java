f600e72317c7e230b81804761bbe47ae2509c55f
parknamjun <eahn.park@gmail.com>
2024-10-30 22:28:08 +0900

    외부 이미지 경로사용

--- src/main/java/kr/giljabi/eip/config/WebConfig.java
+++ src/main/java/kr/giljabi/eip/config/WebConfig.java
@@ -1,14 +1,18 @@
 package kr.giljabi.eip.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${giljabi.eip.filePath}")
+    private String filePath;
+
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:/home/ubuntu/apps/eip/images/");
+                .addResourceLocations(filePath);
     }
 
 }
