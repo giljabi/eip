67225129ec0c342780649fc7b43bb81adf30e22b
Ubuntu <ubuntu@ip-172-26-7-243.ap-northeast-2.compute.internal>
2024-11-01 14:21:23 +0900

    Springboot main은 1개만 존재해야 함

--- src/main/java/kr/giljabi/eip/QuizApplication.java
+++ src/main/java/kr/giljabi/eip/QuizApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class QuizApplication implements CommandLineRunner {
 
     private final QuizLoader quizLoader;
