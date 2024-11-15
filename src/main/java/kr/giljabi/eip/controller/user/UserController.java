c3eb13b07f05f2044d382e391451efac32d3753e
parknamjun <eahn.park@gmail.com>
2024-11-15 14:07:17 +0900

    로그인에서 RequestMapping을 사용하는 이유 설명

--- src/main/java/kr/giljabi/eip/controller/user/UserController.java
+++ src/main/java/kr/giljabi/eip/controller/user/UserController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
 
+    /**
+     * LoginFailureHandler dispatcher.forward(request, response) 원래 요청의 HTTP 메서드(GET, POST 등)를 그대로 유지
+     * RequestMapping를 사용해서 POST/GET을 구분하지 않고, 하나의 메서드로 처리해야 에러메세지를 thymelaf에서 처리할 수 있음
+     * @return
+     */
     @Operation(summary = "로그인 화면으로 이동")
     @RequestMapping("/login")
     public String login(){
