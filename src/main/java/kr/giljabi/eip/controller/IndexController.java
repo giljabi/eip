a6302aa72317fe6fdd51cc879558410b7702f3e2
parknamjun <eahn.park@gmail.com>
2024-12-20 23:14:09 +0900

    index 호출 IP 로그추가

--- src/main/java/kr/giljabi/eip/controller/IndexController.java
+++ src/main/java/kr/giljabi/eip/controller/IndexController.java
@@ -2,13 +2,17 @@ package kr.giljabi.eip.controller;
 
 import kr.giljabi.eip.dto.request.SubjectQuestionDTO;
 import kr.giljabi.eip.service.QNameService;
+import kr.giljabi.eip.util.CommonUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
+@Slf4j
 @Controller
 public class IndexController {
     private final QNameService qNameService;
@@ -19,7 +23,8 @@ public class IndexController {
     }
 
     @RequestMapping("/")
-    public String index(Model model){
+    public String index(HttpServletRequest request, Model model){
+        log.info("index ip: {}", CommonUtils.getClientIp(request));
         List<SubjectQuestionDTO> questions = qNameService.findByQnameCount();
         model.addAttribute("quizzes", questions);
         return "index";
